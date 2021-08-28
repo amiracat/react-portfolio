9d135b4399433dab6002850cb256bdc3927c207d
Amira Holden <amiraholden@gmail.com>
2021-08-27 22:10:16 -0500

    adding npm pkgs

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -8,7 +8,6 @@ import Resume from './pages/Resume';
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
 
-  // TODO: Add a comment describing the functionality of this method
   //function that determines which page will be rendered
 
   const renderPage = () => {
