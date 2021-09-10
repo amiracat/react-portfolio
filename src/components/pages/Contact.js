e369cf8479eae099e5d0a8a2bc91de5ae20ef6ac
Amira Holden <amiraholden@gmail.com>
2021-09-09 22:00:01 -0500

    disabling nonfunctioning form elements

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,21 +8,22 @@ export default function Contact() {
       <Header />
       <div className="content">
         <h2 id="h2">Contact</h2>
-        <p>
-        <form className="blockc">
+        <p> This contact form is under construction. Check back for functionality soon. </p>
+        <p>Please message Amira at <a href="mailto:amiraholden@gmail.com">amiraholden@gmail.com</a>.</p>
+        <p><form className="blockc">
         <label className="blockc">
         Name:
-        <input  className="blockc"type="text" name="name" />
+        <input  className="blockc"type="text" name="name" disabled />
         </label>
         <label className="blockc">
         Email:
-        <input  className="blockc"type="email" name="email" />
+        <input  className="blockc"type="email" name="email" disabled />
         </label>
         <label className="blockc">
         Message:
-        <input className="blockc" type="text" name="message" />
+        <input className="blockc" type="text" name="message" disabled />
         </label>
-        <input className="button" style={{ marginTop: '20px' }} type="submit" value="Submit" />
+        <input className="button" style={{ marginTop: '20px' }} type="submit" value="Submit" disabled />
         </form>
         </p>
       </div>
